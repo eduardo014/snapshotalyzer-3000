bb34e4663753a72fa29c294907c51d8fbbf55211
EDUARDO SUAREZ <eduardo@EDUARDOs-MacBook-Pro.local>
2020-04-16 13:14:38 -0700

    Fix the name of the list_snapshots function

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -47,6 +47,8 @@ def list_snapshots(project):
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+
+                if s.state == "completed": break
     return
 
 
